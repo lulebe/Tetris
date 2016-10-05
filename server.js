cfd10b89004223f21e1ac855369f57266a33a50b
Leander Berg <reg@lulebe.net>
2016-10-05 12:00:19 +0200

    integrated tgbot api

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require('path');
+var TGBot = require('node-telegram-bot-api');
+var constants = require('./constants.json');
+
+var Bot = new TGBot(constants.bottoken);
 
 var app = express();
 
-app.use(express.static('app'));
+app.use(express.static(path.resolve(__dirname, 'app')));
 
 app.listen(3000);
